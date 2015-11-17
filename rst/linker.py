0059b0348daade6d1a6be661318936b654dabb0e
Jason R. Coombs <jaraco@jaraco.com>
2015-11-17 14:23:02 -0500

    Allow for different Repl classes based on the spec indicated.

--- rst/linker.py
+++ rst/linker.py
@@ -1,16 +1,33 @@
 """
-Sphinx plugin to add links to the changelog.
+Sphinx plugin to add links and timestamps to the changelog.
 """
 
 import re
 import os
 import operator
+import subprocess
 
 import six
 
 
 class Repl:
+    @classmethod
+    def from_defn(cls, defn):
+        "Return the first Repl subclass that works with this"
+        instances = (subcl(defn) for subcl in cls.__subclasses())
+        return next(filter(None, instances))
 
+    def __init__(self, defn):
+        vars(self).update(defn)
+
+    def matches(self, text):
+        return re.match(self.pattern+'$', text)
+
+    def __bool__(self):
+        return False
+
+
+class URLLinker(Repl):
     """
     Each replacement should have the form:
 
@@ -23,12 +40,6 @@ class Repl:
     Currently, each named group must be unique across all Repl objects used
     in a replacement.
     """
-    def __init__(self, defn):
-        vars(self).update(defn)
-
-    def matches(self, text):
-        return re.match(self.pattern+'$', text)
-
     def replace(self, match, replacer_vars):
         text = match.group(0)
         ns = match.groupdict()
@@ -36,6 +47,56 @@ class Repl:
         ns.update(replacer_vars)
         hyperlink = '`{text} <{href}>`_'
         return hyperlink.format(text=text, href=self.url.format(**ns))
+
+    def __bool__(self):
+        return 'url' in vars(self)
+
+
+class SCMTimestamp(Repl):
+    """
+    Replace content with a version number to include the date stamp
+    from the SCM.
+
+    For example, consider a changelog with the following:
+
+        1.0
+        ---
+
+        Changed something.
+
+    The following replacement definition would add a datestamp
+    after the heading:
+
+    {
+        pattern: r"^((?P<scm_version>\d+(\.\d+){1,2})\n-+\n)",
+        with_scm: "{text}\nTagged {rev[timestamp]}\n",
+    }
+
+    If the scm_version is detected, a timestamp will be added to the
+    namespace.
+    """
+    def replace(self, match, replacer_vars):
+        text = match.group(0)
+        scm_version = match.group('scm_version')
+        rev = self._get_scm_info_for(scm_version)
+        if not rev:
+            return text
+        ns = match.groupdict()
+        ns.update(vars(self))
+        ns.update(replacer_vars)
+        return self.with_scm.format(**ns)
+
+    @staticmethod
+    def _get_scm_info_for(scm_version):
+        cmd = ['git', 'log', '-1', '--format=%ai', scm_version]
+        try:
+            ts = subprocess.check_output(cmd).decode('utf-8').strip()
+        except Exception:
+            return
+        return dict(timestamp=ts)
+
+    def __bool__(self):
+        return 'with_scm' in vars(self)
 
 
 class Replacer(list):
@@ -54,7 +115,7 @@ class Replacer(list):
         - using: a dictionary of variables available for substitution
         - replace: a list of replacement definitions.
         """
-        repls = map(Repl, defn.get('replace', []))
+        repls = map(Repl.from_defn, defn.get('replace', []))
         self = cls(repls)
         vars(self).update(defn.get('using', {}))
         return self

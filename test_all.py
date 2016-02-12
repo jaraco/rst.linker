0ff83b621cd07dc7ce267afbacdf63a5030fb388
Jason R. Coombs <jaraco@jaraco.com>
2016-02-12 12:38:00 -0500

    Simplify test

--- test_all.py
+++ test_all.py
@@ -4,28 +4,25 @@ import rst.linker
 
 
 @pytest.fixture
-def linker_example():
-    return {
-        'CHANGES.rst': dict(
-            using = dict(
-                kiln='https://org.kilnhg.com/Code/Repositories'
+def linker_defn():
+    return dict(
+        using = dict(
+            kiln='https://org.kilnhg.com/Code/Repositories'
+        ),
+        replace = [
+            dict(
+                pattern=r"proj (?P<proj_ver>\d+(\.\d+)*([abc]\d+)?)",
+                url='{kiln}/repo/proj/Files/CHANGES?rev={proj_ver}',
             ),
-            replace = [
-                dict(
-                    pattern=r"proj (?P<proj_ver>\d+(\.\d+)*([abc]\d+)?)",
-                    url='{kiln}/repo/proj/Files/CHANGES?rev={proj_ver}',
-                ),
-                dict(
-                    pattern=r"(Case |#)(?P<id>\d+)",
-                    url='https://org.fogbugz.com/f/cases/{id}/',
-                ),
-            ],
-        )
-    }
+            dict(
+                pattern=r"(Case |#)(?P<id>\d+)",
+                url='https://org.fogbugz.com/f/cases/{id}/',
+            ),
+        ],
+    )
 
-def test_linker_example(linker_example):
-    defn = linker_example['CHANGES.rst']
-    repl = rst.linker.Replacer.from_definition(defn)
+def test_linker_example(linker_defn):
+    repl = rst.linker.Replacer.from_definition(linker_defn)
     assert 'kilnhg' in repl.run("""
         proj 1.0 was released
         """)

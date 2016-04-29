f0e6a2ea07ce6466967ffbdb5f1cf99bc1428729
Jason R. Coombs <jaraco@jaraco.com>
2016-04-29 10:58:20 -0400

    Remove now superfluous namespace_packages declaration.

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup_params = dict(
 	long_description=long_description,
 	url="https://github.com/jaraco/" + name,
 	packages=setuptools.find_packages(),
-	namespace_packages=['rst'],
 	include_package_data=True,
 	namespace_packages=name.split('.')[:-1],
 	install_requires=[
